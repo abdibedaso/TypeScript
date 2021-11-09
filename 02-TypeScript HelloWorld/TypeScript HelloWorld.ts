8e5755d57ca1cf46be79a075f726d646d69a42fa
Abdi T Bedaso <atbedaso@gmail.com>
2021-11-09 18:39:55 -0500

    tsc helloworld

---
+++ 02-TypeScript HelloWorld/TypeScript HelloWorld.ts
@@ -0,0 +1,11 @@
+// TypeScript
+
+function speak(value: string){
+    document.write(value);
+}
+
+var greeted: string = "World";
+var greeting: string = "hello, ";
+var whatToSay: string = greeting + greeted;
+
+speak(whatToSay); 
