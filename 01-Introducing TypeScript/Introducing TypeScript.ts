548ecf1e09bc76a7c560a29542ed0e7e18db6385
Abdi T Bedaso <atbedaso@gmail.com>
2021-11-09 18:37:14 -0500

    first commit

---
+++ 01-Introducing TypeScript/Introducing TypeScript.ts
@@ -0,0 +1,44 @@
+// TypeScript 
+// TypeScript is an open-source object-oriented language developed and maintained by Microsoft. 
+// It is a typed superset of JavaScript that compiles to plain JavaScript.
+// TypeScript was first released in October 2012.
+// It's the official language adopted by the Google Angular Team to write Angular projects.
+
+// Typed superset of JavaScript programming 
+    //- Adds the concept of static typing to the core futures of js
+    //- Adds helpful and productive features without changing javaScript
+    //- Built for large application, but great for applications of any size
+
+// Dynamic Vs Static Languages
+    // Dynamic                                       //Static
+    // -- No Strict Types                            // -- Strict on Types
+    // -- Forgiving                                  // -- Rigid
+    // -- Great for web browser object model         // -- Promotes stability and maintainability
+
+// Why TypeScript
+    // JavaScript is a dynamic programming language with no type system.
+    // A no type system means that a variable in JavaScript can have any type of value such as string, number, boolean etc.
+    // The type system increases the code quality, readability and makes it an easy to maintain and refactor code base.
+    // Errors can be caught at compile time (development) rather than at run time.
+
+// Why Types?
+    // One of the great things about type checking is that:
+        // 1. It helps writing safe code because it can prevent bugs at compile time.
+        // 2. Compilers can improve and run the code faster.
+    // It’s worth noting that types are optional in TypeScript.
+
+// TypeScript Transpilers - TypeScript Compiler
+   // Tools that transfer code from one language to another!
+    // TypeScript Also refers to the compiler which trans-compiles code written in TS syntax to JS
+    // TypeScript compiles into simple JavaScript.
+    // A TypeScript code is written in a file with .ts extension and then compiled into JavaScript using the TypeScript compiler.
+    // A TypeScript compiler needs to be installed on your platform. 
+    // Once installed, the command tsc filename.ts compiles the TypeScript code into a plain JavaScript file.
+
+// TypeScript Features
+    // Cross-Platform
+    // Object Oriented Language
+    // Static type-checking
+    // Optional Static Typing
+    // DOM Manipulation
+    // ES6/next Features
